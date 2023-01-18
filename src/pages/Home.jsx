eb885c8ef018935276e61d8ab327b76abda84f18
MBANK\aitmukhamed.a <aitmukhamed.a@simple.mn>
2023-01-18 11:39:15 +0800

    a

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import PopularMenu from "../components/popular-menu/PopularMenu";
 import ChooseUs from "../components/choose-us/ChooseUs";
 import MenuPack from "../components/menu-pack/MenuPack";
 import Testimonials from "../components/testimonials/Testimonials";
-import Download from "../components/download-section/Download";
+// import Download from "../components/download-section/Download";
 import Footer from "../components/footer/Footer";
 
 const Home = () => {
